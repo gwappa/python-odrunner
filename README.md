e410e43a257a30b135a4e2d60fed66f686824aaf
Keisuke Sehara <keisuke.sehara@gmail.com>
2019-08-31 16:41:59 +0200

    add setup script

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# python-odrunner
-A python-based tool for helping data logging in an OpenData style
+# odrunner
+
+A python-based tool for helping data logging in an open-data style.
+
+## installation
+
+There is no PyPI upload at the moment...
+
+Option 1: directly from the git repository:
+
+```
+pip install git+https://github.com/gwappa/python-odrunner.git
+```
+
+Option 2: clone and install:
+
+```
+git clone https://github.com/gwappa/python-odrunner.git
+cd python-odrunner
+pip install .
+```
+
+## license
+
+The MIT license
---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+from odrunner import VERSION_STR
+
+setuptools.setup(
+    name='odrunner',
+    version=VERSION_STR,
+    description='A python-based tool for helping data logging in an open-data style',
+    url='https://github.com/gwappa/python-odrunner',
+    author='Keisuke Sehara',
+    author_email='keisuke.sehara@gmail.com',
+    license='MIT',
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        ],
+    packages=setuptools.find_packages(),
+    entry_points={
+        # 'console_scripts': [
+        #     '%module% =%module%.__main__:run'
+        # ]
+    }
+)
